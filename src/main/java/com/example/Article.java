5e45e2322f4ccd15533cb71fbe2206177db66ca5
jiseopshin <wltjq1203@icloud.com>
2025-06-05 12:18:23 +0900

    도메인 객체 작성

--- src/main/java/com/example/Article.java
+++ src/main/java/com/example/Article.java
@@ -1,4 +1,17 @@
 package com.example;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@RequiredArgsConstructor
 public class Article {
+    private final long id;
+    private final String title;
+    private final String body;
+    private final LocalDateTime createdDate;
+    private final LocalDateTime modifiedDate;
+    private final boolean isBlind;
 }
