292563a8f9e36cc26ab7c401447b34e45259039f
jiseopshin <wltjq1203@icloud.com>
2025-06-05 12:16:56 +0900

    SimpleDb클래스 작성

--- src/main/java/com/example/SimpleDb.java
+++ src/main/java/com/example/SimpleDb.java
@@ -1,4 +1,40 @@
 package com.example;
 
-public class simpleDB {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class SimpleDb {
+    private final String address;
+    private final String user;
+    private final String password;
+    private final String database;
+    private final boolean devMode = false;
+
+
+    public void setDevMode(boolean b) {
+    }
+
+    public void run(String query) {
+    }
+
+    public void run(String query, String title, String body, boolean isBlind) {
+    }
+
+    public Sql genSql() {// SQL 객체 반환
+        return new SqlImpl();
+    }
+
+    public void close() {
+    }
+
+    public void startTransaction() {
+    }
+
+    public void commit() {
+    }
+
+    public void rollback() {
+    }
 }
